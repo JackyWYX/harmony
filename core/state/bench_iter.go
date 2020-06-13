0ac26819e8ce3350721760d062523276de737139
Jacky Wang <jackyw.se@gmail.com>
2020-06-12 20:15:46 -0700

    added count to ensure iterate happens

--- core/state/bench_iter.go
+++ core/state/bench_iter.go
@@ -13,12 +13,15 @@ import (
 func (db *DB) BenchIter() {
 	timeStart := time.Now()
 	it := trie.NewIterator(db.trie.NodeIterator(nil))
+
+	i := 0
 	for it.Next() {
+		i++
 		enc := it.Value
 		var acc Account
 		rlp.DecodeBytes(enc, &acc)
 		db.db.ContractCode(crypto.Keccak256Hash(it.Key[:]), common.BytesToHash(acc.CodeHash))
 	}
 	elapsed := time.Since(timeStart)
-	fmt.Printf("elapsed time: %v\n", elapsed)
+	fmt.Printf("elapsed time: %v %v\n", elapsed, i)
 }
